f1d698861f9980cc2a82ea69c5c18ee01cc678ad
K-MishGUN <mishakuchinskiy@gmail.com>
2022-09-16 18:47:29 +0300

    Add Helicopter.java

---
+++ src/transport/flyingTransport/typesOfFlyingTransport/Helicopter.java
@@ -0,0 +1,30 @@
+package transport.flyingTransport.typesOfFlyingTransport;
+
+import transport.flyingTransport.FlyingTransport;
+
+public class Helicopter extends FlyingTransport {
+    @Override
+    public void refueling() {
+
+    }
+
+    @Override
+    public void carWash() {
+
+    }
+
+    @Override
+    public void parking() {
+
+    }
+
+    @Override
+    public void repair() {
+
+    }
+
+    @Override
+    public void MoveOnlySky() {
+
+    }
+}
